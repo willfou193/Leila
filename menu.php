9a001eebf113f61d1f5c5d0db1b0a05bf3cb79f8
William Foucher <e1938665@cmaisonneuve.qc.ca>
2021-10-06 16:57:26 -0400

    Code de démarage du TP#1

--- menu.php
+++ menu.php
@@ -10,6 +10,16 @@
   //Faire appel < la fonction citationAleatoire
 
   $citationAlea = citationAleatoire($page, $langueChoisie);
+
+  /*
+  Gestion de l'affichage dynamique du menu
+  */
+  // Lire le fichier JSON contenant le menu dans la langue courante
+  //utiliser file_exists() si une langue non-traduite est choisie
+  $menuTexte = file_get_contents("data/menu-$langueChoisie.json");
+  // Décoder le code JSON en tableau PHP
+  $menuTableau = json_decode($menuTexte, true);
+  
 ?>
     <div class="contenu-principal">
       <div class="citation">
@@ -20,83 +30,24 @@
         </blockquote>
       </div>
       <div class="carte">
+      <?php  foreach($menuTableau as $TitreSection => $PlatsSection){?>
+
         <section>
-          <h2>Entrées</h2>
+          <h2><?= $TitreSection; ?></h2>
           <ul>
+           <?php foreach($PlatsSection as $plat){ ?>
+
             <li>
-              <span>Escargots à la crème d’ail</span>
-              <span class="prix">31</span>
+              <span><?= $plat['nom']; ?><br><i><?= $plat['desc']; ?></i></span>
+              <span class="prix"><i class="article-menu-portion"><?= $mnu_portions; ?><?= $plat['portion']; ?></i><?= $plat['prix']; ?></span>
             </li>
-            <li>
-              <span>Foie gras de canard poêlé aux coings<br><i>gâteau et infusion de coing à la verveine</i></span>
-              <span class="prix"><i class="article-menu-portion">(pour 2 personnes)</i>34</span>
-            </li>
-            <li>
-              <span>Jardin de champignons d’automne<br><i>crème de cèpes, émulsion au thé noir</i></span>
-              <span class="prix">33</span>
-            </li>
+
+            <?php } ?>
           </ul>
         </section>
-        <section>
-          <h2>Poissons</h2>
-          <ul>
-            <li>
-              <span>Sandre à la peau croustillante<br><i>fondue d’échalotes, sauce au vin rouge</i></span>
-              <span class="prix">42</span>
-            </li>
-            <li>
-              <span>Saint-pierre rôti aux olives taggiasche<br><i>mousseline d’artichaut, fumet de poisson au citron kalamansi</i></span>
-              <span class="prix">49</span>
-            </li>
-            <li>
-              <span>Bar cuit à la vapeur et criste marine<br><i>déclinaison de riz et coquillages, jus au curcuma</i></span>
-              <span class="prix"><i class="article-menu-portion">(pour 2 personnes)</i>58</span>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>Viandes</h2>
-          <ul>
-            <li>
-              <span>Côte de porcelet et poitrine de cochon du Cantal croustillant<br><i>légumes de saison et crémeux de céleri-rave</i></span>
-              <span class="prix">42</span>
-            </li>
-            <li>
-              <span>Filet de canette rôti sur la peau, jus au porto infusé à l’hibiscus<br><i>tartelette de figues et cuisse confite, petite chartreuse de figues</i></span>
-              <span class="prix">44</span>
-            </li>
-            <li>
-              <span>Ris de veau doré au sautoir et cèpes<br><i>mousseline de cèpes, jus de veau à la cazette du Morvan</i></span>
-              <span class="prix">63</span>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>Fromages</h2>
-          <ul>
-            <li>
-              <span>Chariot de fromages affinés de nos régions</span>
-              <span class="prix">12</span>
-            </li>
-          </ul>
-        </section>
-        <section>
-          <h2>Desserts</h2>
-          <ul>
-            <li>
-              <span>Poire Louise Bonne pochée au citron doux<br><i>parfait glacé à la Nepeta, crumble de petit épeautre</i></span>
-              <span class="prix">13</span>
-            </li>
-            <li>
-              <span>Fleur de cassis de Bourgogne<br><i>chiboust à la vanille et chocolat grand cru de Madagascar</i></span>
-              <span class="prix">13</span>
-            </li>
-            <li>
-              <span>Tarte fine aux pommes de Bernard Loiseau<br><i>sorbet pomme verte</i></span>
-              <span class="prix">12</span>
-            </li>
-          </ul>
-        </section>
+
+      <?php } ?>
+        
       </div>
     </div>
 <?php
